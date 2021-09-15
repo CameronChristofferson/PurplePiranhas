e6b4253aba373bd2280246e6fa6203549d6de06b
Cameron Christofferson <79726318+CameronChristofferson@users.noreply.github.com>
2021-09-15 14:40:18 -0600

    Update group.py

---
+++ group.py
@@ -0,0 +1,11 @@
+member1 = []
+member2 = []
+
+def groupMember(member):
+  print(f'Name: {member[0]}')
+  print(f'Year: {member[1]}')
+  print(f'Major: {member[2]}')
+  print(f'Hobbies: {member[3]}')
+  print(f'Hometown: {member[4]}')
+  print(f'Anything else: {member[5]}')
+
